0bc07ad858492f8eaf50d99483fe7dcc49aafd2f
Lucas Kanashiro <kanashiro.duarte@gmail.com>
2016-01-06 15:14:10 -0200

    Refactor copr_info subroutine

--- lib/CoprStatus.pm
+++ lib/CoprStatus.pm
@@ -7,42 +7,37 @@ use LWP::UserAgent;
 
 $ENV{'PERL_LWP_SSL_VERIFY_HOSTNAME'} = 0;
 
+# hash with repos data
+our $info = {};
+
 sub copr_monitor_url {
   my ( $user, $repo ) = @_;
   return "http://copr.fedoraproject.org/api/coprs/$user/$repo/monitor/";
 }
 
 sub copr_info {
+  my ( $user, $repo, $branch ) = @_;
+
   my $ua = LWP::UserAgent->new;
   $ua->timeout(300);
   $ua->env_proxy;
   $ua->ssl_opts(SSL_verify_mode => 0x00);
 
-  my $result_v4 = $ua->get(copr_monitor_url("softwarepublico", "v4"));
-  my $result_v5 = $ua->get(copr_monitor_url("softwarepublico", "v5"));
+  my $result = $ua->get(copr_monitor_url($user, $repo));
 
   my $json = JSON->new->allow_nonref;
 
-  my $dec_result_v4 = $json->decode($result_v4->decoded_content);
-  my $dec_result_v5 = $json->decode($result_v5->decoded_content);
-  my $info = {};
+  my $dec_result = $json->decode($result->decoded_content);
 
-  foreach(@{$dec_result_v4->{'packages'}}) {
+  foreach(@{$dec_result->{'packages'}}) {
     my $package = $_->{'pkg_name'};
     my $status = $_->{'results'}{'epel-7-x86_64'}{'status'};
     my $version = $_->{'results'}{'epel-7-x86_64'}{'pkg_version'};
-    $info->{$package}->{'v4_version'} = $version if $status eq "succeeded";
-  }
-
-  foreach(@{$dec_result_v5->{'packages'}}) {
-    my $package = $_->{'pkg_name'};
-    my $status = $_->{'results'}{'epel-7-x86_64'}{'status'};
-    my $version = $_->{'results'}{'epel-7-x86_64'}{'pkg_version'};
-    $info->{$package}->{'v5_version'} = $version if $status eq "succeeded";
+    $info->{$package}->{$repo."_version"} = $version if $status eq "succeeded";
   }
 
   foreach my $key (keys %{$info}) {
-    my $spec = $ua->get("https://softwarepublico.gov.br/gitlab/softwarepublico/softwarepublico/raw/master/src/pkg-rpm/$key/$key.spec");
+    my $spec = $ua->get("https://softwarepublico.gov.br/gitlab/softwarepublico/softwarepublico/raw/$branch/src/pkg-rpm/$key/$key.spec");
     my $version = $1 if $spec->decoded_content =~ /^Version:\s*([^\s]+)\s*$/m;
     if($version =~ /%\{version\}/) {
       $version = $1 if $spec->decoded_content =~ /define version\s*([^\s]+)\s*$/m;
@@ -51,27 +46,14 @@ sub copr_info {
     my $release = 'no_release';
     $release = $1 if $spec->decoded_content =~ /^Release:\s*([^\s]+)\s*$/m;
     $version = "$version-$release";
-    $info->{$key}->{'git_version_master'} = $version;
+    $info->{$key}->{'git_version_'.$branch} = $version;
   }
 
-  foreach my $key (keys %{$info}) {
-    my $spec = $ua->get("https://softwarepublico.gov.br/gitlab/softwarepublico/softwarepublico/raw/stable-4.x/src/pkg-rpm/$key/$key.spec");
-    my $version = $1 if $spec->decoded_content =~ /^Version:\s*([^\s]+)\s*$/m;
-    if($version =~ /%\{version\}/) {
-      $version = $1 if $spec->decoded_content =~ /define version\s*([^\s]+)\s*$/m;
-    }
-
-    my $release = 'no_release';
-    $release = $1 if $spec->decoded_content =~ /^Release:\s*([^\s]+)\s*$/m;
-    $version = "$version-$release";
-    $info->{$key}->{'git_version_stable_4'} = $version;
-  }
-
-  return $info;
 }
 
 sub compare_versions {
-  my $info = copr_info();
+  copr_info('softwarepublico', 'v4', 'stable-4.x');
+  copr_info('softwarepublico', 'v5', 'master');
   my $match = {};
   foreach my $key (keys %{$info}) {
     if($info->{$key}->{'v5_version'} eq $info->{$key}->{git_version_master}) {
@@ -86,12 +68,13 @@ sub compare_versions {
 }
 
 sub info2html {
-  my $info = copr_info();
+  copr_info('softwarepublico', 'v4', 'stable-4.x');
+  copr_info('softwarepublico', 'v5', 'master');
   my $table_entries="";
   foreach my $key (keys %{$info}) {
     my $fill_v4_row;
     my $fill_v5_row;
-    if($info->{$key}->{'v4_version'} eq $info->{$key}->{git_version_stable_4}) {
+    if($info->{$key}->{'v4_version'} eq $info->{$key}->{'git_version_stable-4.x'}) {
       $fill_v4_row = "success";
     }
     else {
@@ -107,7 +90,7 @@ sub info2html {
 
     $table_entries .= "<tr>
     <td><b>$key</b></td>
-    <td>$info->{$key}->{'git_version_stable_4'}</td>
+    <td>$info->{$key}->{'git_version_stable-4.x'}</td>
     <td class=\"$fill_v4_row\">$info->{$key}->{'v4_version'}</td>
     <td>$info->{$key}->{'git_version_master'}</td>
     <td class=\"$fill_v5_row\">$info->{$key}->{'v5_version'}</td>
@@ -147,7 +130,8 @@ sub serve_html {
 };
 
 sub serve_json {
-  my $info = copr_info();
+  copr_info('softwarepublico', 'v4', 'stable-4.x');
+  copr_info('softwarepublico', 'v5', 'master');
   my $json = JSON->new->allow_nonref;
   my $json_info = $json->encode($info);
   return [
